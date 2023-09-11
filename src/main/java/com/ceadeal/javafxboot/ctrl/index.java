aa11e2c30cd6a3c2be25a9bb72d6cb3e9448a55e
Lt <longtao0426@163.com>
2023-09-11 20:23:31 +0800

    java fx

--- src/main/java/com/ceadeal/javafxboot/ctrl/index.java
+++ src/main/java/com/ceadeal/javafxboot/ctrl/index.java
@@ -1,15 +1,15 @@
 package com.ceadeal.javafxboot.ctrl;
 
 import de.felixroske.jfxsupport.FXMLController;
-import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,7 +19,7 @@ import java.util.ResourceBundle;
  */
 @Slf4j
 @FXMLController
-public class index extends AnchorPane implements Initializable {
+public class index implements Initializable {
     // 主容器
     public Pane rootIndex;
 
@@ -28,5 +28,20 @@ public class index extends AnchorPane implements Initializable {
         log.info("initialize: {}", location.getPath());
     }
 
+    public void btn1(ActionEvent actionEvent) {
+        Window window = rootIndex.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        // 文件类型过滤器
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Excel files (*.xls, *.xlsx)", "*.xls", "*.xlsx");
+        fileChooser.getExtensionFilters().add(filter);
+        // ...
+        File file = fileChooser.showOpenDialog(window);
+        String fileName = file == null ? "" : file.getName();
+        String fileAbsolutePath = file == null ? "" : file.getAbsolutePath();
+
+        if (file != null) {
+            
+        }
+    }
 
 }
